6d123df4b7fdb23f9bc82f0a011ce30a0c6ecddd
revankarveena <52241596+revankarveena@users.noreply.github.com>
2019-07-10 14:14:15 +0530

    Add files via upload

---
+++ DataStoreManager.java
@@ -0,0 +1,35 @@
+package org.veena.miniproject;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DataStoreManager {
+
+	public void serializeStudent() {
+		
+	}
+	
+	public ConcurrentHashMap<Integer,Student> deserializeStudent(){
+		
+	}
+	
+	public int loadFileConfiguration() {
+		int flag = 0;
+		
+		return  flag;
+	}
+	
+	public void add(Student student,ConcurrentHashMap<Integer,Student> champ) {
+		
+	}
+	public void delete(Integer studentId,ConcurrentHashMap<Integer,Student> champ) {
+		
+	}
+	public void edit(Integer studentId,String name,ConcurrentHashMap<Integer,Student> champ) {
+		
+	}
+	public void view(ConcurrentHashMap<Integer,Student> champ) {
+	
+	}
+	
+	
+}
