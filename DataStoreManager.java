b1d78b587b5a6132b38298e6d3d02e6c978a646a
revankarveena <52241596+revankarveena@users.noreply.github.com>
2019-07-10 16:21:34 +0530

    Update DataStoreManager.java

--- DataStoreManager.java
+++ DataStoreManager.java
@@ -4,33 +4,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class DataStoreManager {
 
-	public void serializeStudent(ConcurrentHashMap<Integer,Student> cHashMap) {
-		
-	}
 	
-	public ConcurrentHashMap<Integer,Student> deserializeStudent(){
-		return null;
-		
-	}
-	
-	public int loadConfigurationFile() {
-		int flag = 0;
-		
-		return  flag;
-	}
-	
-	public void add(Student student,ConcurrentHashMap<Integer,Student> champ) {
-		
-	}
-	public void delete(Integer studentId,ConcurrentHashMap<Integer,Student> champ) {
-		
-	}
-	public void edit(Integer studentId,String name,ConcurrentHashMap<Integer,Student> champ) {
-		
-	}
-	public void view(ConcurrentHashMap<Integer,Student> champ) {
-	
-	}
 	
 	
 }
